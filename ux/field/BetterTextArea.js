a961c4cead8f4eee30a83fb91021f9bd22699af0
Colin Ramsay <colin@gotripod.com>
2012-05-08 17:42:14 +0100

    Change the scroll direction

--- ux/field/BetterTextArea.js
+++ ux/field/BetterTextArea.js
@@ -72,7 +72,7 @@ Ext.define('Ext.ux.field.BetterTextArea', {
 
 
 	onTouchMove: function(e) {
-		this.editorPanel.down('textareafield').element.down('textarea').dom.scrollTop += (e.deltaY / 2.0);
+		this.editorPanel.down('textareafield').element.down('textarea').dom.scrollTop -= (e.deltaY / 2.0);
 	},
 
 
